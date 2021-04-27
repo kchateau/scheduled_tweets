d1c3c1c3ebe827b4b67fd8beb554a3227ebcf87d
Kendra Chateau <kendra@super.gd>
2021-04-27 07:44:22 -0700

    Add logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
+  delete "logout", to: "sessions#destroy"
 
   # This is the root - the '/' is optional, all the below is the same(?)
   # get "", to: "main#index"
